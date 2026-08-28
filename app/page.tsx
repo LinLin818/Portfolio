b69f1cfc66ac873a400c5aad0da95d26ca291cc9
LinLin818 <ll6638409@gmail.com>
2026-08-27 22:22:13 -0400

    Adding personal interest

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@
 export default function Home() {
   return (
     <>
-    <p> Hi! My name is Lin Lin. I am currently an upcoming junior at Drexel pursuing Computer Science</p>
+    <p> Hi! My name is Lin Lin. I am currently an upcoming junior at Drexel pursuing Computer Science.</p>
+    <p> Outside of school, I like hangout with friends, trying new food and journaling.</p>
+    <p> I like to listen to music, watch financial literacy videos and dooming scrolling on Tiktok. </p>
     </>
   );
 }
+ 
