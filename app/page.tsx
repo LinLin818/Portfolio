d9d3a2691dc68f3a88bea4de9801a19a1c80d83d
LinLin818 <ll6638409@gmail.com>
2026-09-03 22:12:00 -0400

    Server testing

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <p> Hi! My name is Lin Lin. I am currently an upcoming junior at Drexel pursuing Computer Science.</p>
     <p> Outside of school, I like hangout with friends, trying new food and journaling.</p>
     <p> I like to listen to music, watch financial literacy videos and dooming scrolling on Tiktok. </p>
+    <p> Testing</p>
     </>
   );
 }
